a893199846e1bf2cdcb34510ab59284d850abf91
Umut Göler <umutgoler@gmail.com>
2024-05-07 18:24:01 +0300

    Add default constructor to Task

--- src/Task.java
+++ src/Task.java
@@ -1,10 +1,14 @@
 public final class Task {
-    public static final float defaultSize = 3.f;
+    public static final float defaultSize = 1.f;
 
     public final String type;
     public final float size;
 
-    public Task(String Type, int Size, String type, float size) {
+    public Task(String type) {
+        this(type, defaultSize);
+    }
+
+    public Task(String type, float size) {
         this.type = type;
         this.size = size;
     }
